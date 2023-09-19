2d9a78979ce493c7614e213869a593ee6fc5f758
Tranhuy1612 <huytran1612@icloud.com>
2023-09-20 02:57:39 +0700

    MD3_ss4bt

--- src/bt4/TimPTLonNhatMang2Chieu.java
+++ src/bt4/TimPTLonNhatMang2Chieu.java
@@ -5,7 +5,17 @@ import java.util.Arrays;
 public class TimPTLonNhatMang2Chieu {
     public static void main(String[] args) {
 
-        int[][] arr2 = {{1, 2, 3, 3}, {9, 15, 6, 2}, {5, 6, 3, 4}}; // tạo mảng 2 chiều cho trước
+        int[][] arr2 = {
+                {1, 2, 3, 3},
+                {9, 8, 6, 2},
+                {5, 6, 3, 4}
+        };
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
         int max = arr2[0][0];  // tạo biến max =  phần tử thứ 0.0 của mảng
 
         for (int i = 0; i < arr2.length; i++) {   // duyệt mảng 2 chiều
@@ -15,7 +25,6 @@ public class TimPTLonNhatMang2Chieu {
                 }
             }
         }
-        System.out.println("Mảng 2 chiều là : " + arr2);
         System.out.println("phẩn tử lớn nhất trong mảng 2 chiều cho sẵn là : " + max);
     }
 }
